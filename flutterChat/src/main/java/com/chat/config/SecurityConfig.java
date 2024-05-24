6e858cfd2b13808b62fad56eb6e87119fd1f3914
임종윤 <vavau1993@gsitm.com>
2024-05-24 17:18:27 +0900

    Add All project File

---
+++ flutterChat/src/main/java/com/chat/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.chat.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+/**
+ * @author : 임종윤
+ * @version : 1.0
+ * @packageName : com.chat.config
+ * @fileName : SecurityConfig
+ * @date : 2024-05-24
+ * @description : 설명
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/chat", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                );
+        return http.build();
+    }
+
+/*    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }*/
+}
---
+++ flutterChat/src/test/java/com/chat/flutterChat/FlutterChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.flutterChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
